b039c8571e256eec71433cd3f5bd52f6fa364ec9
gitproman <gitproman@gmail.com>
2024-12-06 16:31:08 +0530

    Update update.py

--- update.py
+++ update.py
@@ -4,7 +4,7 @@ from subprocess import run as srun
 
 # Retrieve the upstream repository and branch
 UPSTREAM_REPO = "https://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@github.com/gitproman/storagebot"
-UPSTREAM_BRANCH = "main"
+UPSTREAM_BRANCH = "2nd"
 
 # Validate the repository URL
 if not UPSTREAM_REPO:
